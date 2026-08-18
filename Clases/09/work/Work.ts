f8710321d7eeb810e789006b2745218298d5e1a3
AlejandroMirandaGomez <alejandro.obed.miranda@gmail.com>
2026-08-18 17:28:42 -0600

    Clase 09: work con dfa.ts, tools.ts y configuracion de proyecto

---
+++ Clases/09/work/Work.ts
@@ -0,0 +1,24 @@
+import { $$ } from "./tools.ts"
+
+// import "./test.ts"
+
+import "./dfa.ts"
+
+// type Fun<T> = (x: T) => T
+
+// // Infiere el tipo de retorno (number)
+// function apply<T>(f: Fun<T>, x: T) {
+// return f(x)
+// }
+
+// $$(((x) => x ** 2 + 1)(10))
+
+// $$(apply((x) => "x**2+1", "10"))
+
+// $$(apply((x) => x ** 2 + 1, 10))
+
+// function f(x: number) {
+// return x ** 2 + 1
+// }
+
+// $$(apply(f, 10))
---
+++ Clases/09/work/dfa.ts
@@ -0,0 +1,52 @@
+import { $$ } from "./tools.ts"
+
+import { inspect } from "node:util"
+
+$$("~~".repeat(10))
+
+const black = " "
+
+const dfa_0 = {
+    vocabulary: [black, "1", "2"],
+    states: ["A", "B", "C"],
+    start: "A",
+    final: ["C"],
+    moves: [
+        ["A", black, "B"],
+        ["A", 0, "C"],
+        ["A", 1, "C"],
+        ["B", black, "B"],
+        ["B", 0, "C"],
+        ["B", 1, "C"],
+        ["C", 0, "C"],
+        ["C", 1, "C"],
+    ],
+}
+
+function dfaStatePairsOperativo(dfa, state) {
+    const pairs = []
+    for (const triple of dfa.moves) {
+        if (state === triple[0]) pairs.push(triple[1], triple[2])
+    }
+
+    return pairs
+}
+
+function dfaStatePairs(dfa, state) {
+    return dfa.moves
+        .filter(([f, w, t]) => f === state)
+        .map(([f, w, t]) => [w, t])
+}
+
+$$("dfaStatePairs(dfa_0, A)", dfaStatePairs(dfa_0, "A"))
+
+function dfaAllStatePairs(dfa) {
+    return dfa.states.map((state) => [state, dfaStatePairs(dfa, state)])
+}
+
+$$(
+    "dfaStatePairs(dfa_0, A)",
+    inspect(dfaAllStatePairs(dfa_0, "A"), { depth: 3, colors: true }),
+)
+
+$$("~~".repeat(10))
