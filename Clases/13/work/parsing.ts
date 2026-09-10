3d1e4832f60796af944b71d0c4eec7d9cf6884de
AlejandroMirandaGomez <alejandro.obed.miranda@gmail.com>
2026-09-10 09:15:57 -0600

    Clase 13: agrega parser Or y ajustes en Number/Id

--- Clases/13/work/parsing.ts
+++ Clases/13/work/parsing.ts
@@ -17,7 +17,6 @@ $$("Hello from parsing")
 //////////////// Types ////////////////////
 
 type TResult = "OK" | "FAIL"
-
 type Ok<I, T> = {
     _type: TResult
     token: T
@@ -29,7 +28,7 @@ type Fail<R> = {
     reason: R
 }
 
-type Result<I, T, R> = ok<I, T> | Fail<R>
+type Result<I, T, R> = Ok<I, T> | Fail<R>
 
 const Ok = <I, T>(token: T, rest: I) => ({
     _type: "OK",
@@ -55,23 +54,21 @@ const InputString = (input: string, index: number = 0) => ({
 })
 
 type TToken = "NUM" | "ID"
-
 type Token = {
     _type: TToken
     value: string
 }
 
-const Token = (_type: TToken, value: string) => ({
+const Token = (value: string, _type: TToken) => ({
     _type,
     value,
 })
-
-//////////////////// Parser //////////////////////
+///////////PARSERS///////////
 
 function Number() {
-    //const re_num = /^( | \n | \t |\r)* $/
-    const re_num = /\s*(?<token>\d+)/y
+    const re_num = /^\s*(?<token>\d+)/y // /\s*/ es equivalente a new RegExp("\s*")
     const re_delim = /\W/y
+
     function parser(source: InputString) {
         const { input, index } = source
         re_num.lastIndex = index
@@ -81,52 +78,76 @@ function Number() {
 
         const { token } = match?.groups
         re_delim.lastIndex = re_num.lastIndex
-
         if (re_delim.lastIndex < input.length && !re_delim.test(input))
             return Fail("Invalid delimiter after number")
 
-        return Ok(Token("NUM", token), InputString(input, re_num.lastIndex))
+        return Ok(Token(token, "NUM"), InputString(input, re_num.lastIndex))
     }
+    //
     return parser
 }
 
-function id() {
-    //const re_num = /^( | \n | \t |\r)* $/
-    const re_id = /\s*(?<token>[a-zA-Z_]\w+)/y
+function Id() {
+    const re_id = /^\s*(?<token>[a-zA-Z_]\w*)/y // /\s*/ es equivalente a new RegExp("\s*")
 
     function parser(source: InputString) {
         const { input, index } = source
         re_id.lastIndex = index
         const match = re_id.exec(input)
 
-        if (!match) return Fail("No id could be matched")
+        if (!match) return Fail("No identifier could be matched")
 
         const { token } = match?.groups
 
-        return Ok(Token("ID", token), InputString(input, re_id.lastIndex))
+        return Ok(Token(token, "ID"), InputString(input, re_id.lastIndex))
     }
     return parser
 }
 
+function Or(leftParser, rightParser) {
+    function parser(source: InputString) {
+        const result = leftParser(source)
+        if (!isFail(result)) return result
+        return rightParser(source)
+    }
+    return parser
+}
+
+//////////////// Testing //////////////////////
+
 function test_0() {
-    $$("_".repeat(50))
-    const inputs = ["", "123 abc ", "\n  123 abc", "123abc"]
+    $$("-".repeat(80))
+    const inputs = ["", "123 abc", "\n  123 abc  ", "123abc"]
     const numParser = Number()
+
     for (const input of inputs) {
         const result = numParser(InputString(input))
-        if (result) $$("Testing", input, " => ", result)
+        $$(`Testing ${input}`, result)
     }
 }
 
 function test_1() {
-    $$("_".repeat(50))
-    const inputs = ["", "abc * (", "\n abc 123", "  aBc_123 456", "123abc"]
-    const idParser = id()
+    $$("-".repeat(80))
+    const inputs = ["", "abc * (", "\n  123 abc  ", "  aBc_123 456", "123abc"]
+    const idParser = Id()
+
     for (const input of inputs) {
         const result = idParser(InputString(input))
-        if (result) $$("Testing", input, " => ", result)
+        $$(`Testing ${input}`, result)
+    }
+}
+
+function test_2() {
+    $$("-".repeat(80))
+    const inputs = ["", "abc * (", "\n  123 abc  ", "  aBc_123 456", "123 abc"]
+    const orParser = Or(Id(), Number())
+
+    for (const input of inputs) {
+        const result = orParser(InputString(input))
+        $$(`Testing ${input}`, result)
     }
 }
 
 test_0()
 test_1()
+test_2()
