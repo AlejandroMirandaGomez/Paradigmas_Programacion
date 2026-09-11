6ee8df4a78f45a007b894669c16d5932f7154e3f
AlejandroMirandaGomez <alejandro.obed.miranda@gmail.com>
2026-09-11 11:49:26 -0600

    EjemplosClaseTypescript: notas sobre combinadores de predicados (fooMal vs foo1/foo2)

--- Autoestudios/EjemplosClaseTypescript/work/Work.ts
+++ Autoestudios/EjemplosClaseTypescript/work/Work.ts
@@ -1,8 +1,21 @@
+// Esto no se entiende:
+function fooMal<T>( x: (a: T) => boolean, y: (a: T) => boolean): (a: T[]) => T[] {
+    return (a: T[]): T[] => a.filter(x).filter(y)
+}
+
+// Es mejor asi:
+
 type pred<T> = (a: T) => boolean
 
-function foo<T>(x: pred<T>, y: pred<T>) {
+function foo1<T>(x: pred<T>, y: pred<T>) {
     return (a: T[]) => a.filter(x).filter(y)
 }
+
+// o asi:
+
+const foo2 = <T>(x: pred<T>, y: pred<T>) => (a: T[]) => a.filter(x).filter(y)
+
+////////////////////////////////////////////////////////////////////////////////
 
 let nombre = "Alejandro"
 
