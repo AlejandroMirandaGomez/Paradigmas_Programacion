a95bb92d5de27b07ba33b98c1dbd5b39a48ad5ca
AlejandroMirandaGomez <alejandro.obed.miranda@gmail.com>
2026-09-19 21:44:57 -0600

    Elimina work de Clase 18 (movido a Autoestudios)

--- Clases/18/work/Work.ts
+++
@@ -1,7 +0,0 @@
-import { $$ } from "./tools.ts"
-
-// import "./test.ts"
-
-// import "./dfa.ts"
-
-import "./parsing.ts"
--- Clases/18/work/tools.ts
+++
@@ -1,5 +0,0 @@
-// variadico: variedad variable: Revibe cualquier cantidad de argumentos.
-
-export function $$(...args: any[]) {
-    console.log(...args)
-}
